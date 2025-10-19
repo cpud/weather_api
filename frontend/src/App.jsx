2f55577574a21d21fd8f6fc850688d3d88036a7e
cpud <pudnec14@outlook.com>
2025-10-19 00:13:43 -0400

    set up weather card and have it displaying weather data. next will be redis setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,31 @@
 import {useEffect, useState} from 'react';
 import './App.css';
 import axios from 'axios';
+import WeatherCard from './components/WeatherCard';
 
 const baseUrl = "http://127.0.0.1:8000"
 
 function App() {
   const [location, setLocation] = useState("");
-  const [weather, setWeather] = useState([]);
+  //const [weather, setWeather] = useState([]);
+  const [weather, setWeather] = useState({
+    tempMin: '',
+    tempMax:'',
+    conditions: '',
+    humidity: '',
+  });
 
   const handleSubmit = async(e) => {
     e.preventDefault();
     const data = await axios.get(`${baseUrl}/weather/${location}`);
     const weatherData = data.data
-    console.log(weatherData)
-    //setWeather(weatherData.address) - works!
-    //setWeather(weatherData.description) - also works
-    setWeather(weatherData.days[0].tempmax) // working!!
+    const use = {
+      tempMin: weatherData.days[0].tempmin,
+      tempMax: weatherData.days[0].tempmax,
+      conditions: weatherData.days[0].conditions,
+      humidity: weatherData.days[0].humidity,
+    }
+    setWeather(use) 
   }
 
   const handleChange = e => {
@@ -37,9 +47,9 @@ function App() {
              value={location}/>
              <button type="submit">Submit</button>
           </form>
+          <WeatherCard weather = {weather} />
         </section>
         <section>
-          {weather}
         </section>
       </div>
     </>
@@ -47,3 +57,13 @@ function App() {
 }
 
 export default App
+/*
+          <h4>Max temp</h4> 
+            {weather.tempMax}
+          <h4>Min temp</h4>
+            {weather.tempMin}
+          <h4>Conditions</h4>
+            {weather.conditions}
+          <h4>Humidity</h4>
+            {weather.humidity}
+*/
---
+++ frontend/src/components/WeatherCard.jsx
@@ -0,0 +1,17 @@
+import "../WeatherCard.CSS"
+
+function WeatherCard({ weather }) {
+
+
+	return <div className="weather-card">
+		<div className="weather-info">
+		  <h4>Max temp: {weather.tempMax}</h4> 
+          <h4>Min temp: {weather.tempMin}</h4>
+          <h4>Conditions: {weather.conditions}</h4>
+          <h4>Humidity: {weather.humidity}</h4>
+		</div>
+		
+	</div>
+}
+
+export default WeatherCard
